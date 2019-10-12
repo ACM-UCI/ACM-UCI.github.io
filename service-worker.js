3830d9cac450d0e43f62acc330bc4387562a21a8
Meta Novitia <mnovitia@uci.edu>
2019-10-11 17:05:16 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7afacd840374cd394ab264e3976d6ff2.js"
+  "/precache-manifest.3d52873d0a49ed5199d460ed89aa3102.js"
 );
 
 workbox.clientsClaim();

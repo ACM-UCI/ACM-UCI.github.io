6bbc9e0e2c4d6f853ed580a1128517a3d664d006
btjanaka <bryon.tjanaka@gmail.com>
2019-12-28 22:54:42 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7928a79879f95849f6efb4decba8a4a6.js"
+  "/precache-manifest.0693c19e300c5bc451e949f70f75869e.js"
 );
 
 workbox.clientsClaim();

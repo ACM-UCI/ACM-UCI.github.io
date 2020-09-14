5aaccc0c485969910239c5c0b934813d6e50f8b5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-14 06:04:38 +0000

    Deploy acm-uci/acm-uci.github.io to github.com/acm-uci/acm-uci.github.io.git:master

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.67fcf47a51a5eb879b821499f7b9ecac.js"
+  "/precache-manifest.a54717802614654c8b2e02a7739545c6.js"
 );
 
 self.addEventListener('message', (event) => {

d1030760af0aa286489ec33626a4f8475475582d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-04-07 23:22:24 +0000

    Deploy acm-uci/acm-uci.github.io to github.com/acm-uci/acm-uci.github.io.git:master

--- static/media/i_caught_the_bomber.449fff4d.py
+++
@@ -1,5 +0,0 @@
-N, M = map(int, input().split())
-schools = {input()+'.edu' for _ in range(M)}
-for email in (input() for _ in range(N)):
-    if email.split('@')[1] not in schools:
-        print(email)
